06d60a70bfed3572556a7e5fd4b38f2dbfa73cc3
Suraj Mourya <mouryasuraj6266444@gmail.com>
2025-08-19 23:42:06 +0530

    started warpup insection and completed the warmup introduction

---
+++ warm-up-introduction/notes.js
@@ -0,0 +1,15 @@
+/*
+
+
+1. First of all, DSA is language-agnostic
+2. We will going to use Javascript Programming Language to solve DSA questions
+3. Steps to solve DSA:
+    --> Read the question carefully (read again if you don't understand)
+    --> Think about the approach to solve the question and write it down on paper.
+    --> After getting the idea, write the code and run it.
+    --> Then try to optimize the solution if possible.
+    --> Finally, check the Time and Space complexity of the code.
+
+
+
+*/
