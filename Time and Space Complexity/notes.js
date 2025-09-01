d07fc1c2f573df30ce740bc44e7c1fa4013bf526
Suraj Mourya <mouryasuraj6266444@gmail.com>
2025-09-02 01:48:02 +0530

    Time complexity completed

---
+++ Time and Space Complexity/notes.js
@@ -0,0 +1,19 @@
+/*
+
+Time Complexity - It is used to measure the algorithm running time as the input grows
+
+1. Time Complexity !== Time taken
+2. To represent time complexity we use Big O notation
+3. Big O natation give the worst case, because comparing worse case give the efficient algorithm.
+4. Common Time Complexity
+    1. O(n)
+    2. O(log n)
+    3. O(n log n)
+    4. O(n square)
+    5. 0(2 raised to n)
+    6. O(1) - Constaint Time COmplexity - Best
+    7. O(n!)
+5. Efficiency
+    O(1) > O(log n) > O(n) > O(n log n) > O(n sqaure) > O(2 raised to n) > O(n!)
+
+*/
