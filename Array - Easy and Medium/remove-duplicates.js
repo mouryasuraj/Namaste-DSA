d6713c64484941aae104eb22bf0404ae9ee0a1f1
Suraj Mourya <mouryasuraj6266444@gmail.com>
2025-09-04 01:26:23 +0530

    Complete the question 'Remove duplicate element from an Integer array'

---
+++ Array - Easy and Medium/remove-duplicates.js
@@ -0,0 +1,15 @@
+const nums = [0,0,1,1,2,2,3,4,4,5]
+
+function removeDuplicates(nums){
+    let x = 0;
+    for(let i=0; i<nums.length; i++){
+        if(nums[i]>nums[x]){
+            x++;
+            nums[x] = nums[i]
+        }
+    }
+    return x+1
+}
+
+const result = removeDuplicates(nums)
+console.log(result);
