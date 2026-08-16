eebad032f52e86146c099514930d133da6578567
Suraj Mourya <mouryasuraj6266444@gmail.com>
2026-08-16 15:44:36 +0530

    power of two with two approaches

---
+++ 5. Recursion - Easy and Medium/power-of-two.js
@@ -0,0 +1,30 @@
+// Power of two
+
+// 1st Approach
+// const isPowerOfTwo = (n, i=1 ) =>{
+
+    
+
+//     if(n===1) return true
+
+//     if(Math.pow(2,i)>n) return false
+
+//     if(Math.pow(2,i)===n) return true
+
+//     return isPowerOfTwo(n,i+1)
+// }
+
+// console.log(isPowerOfTwo(5))
+
+const isPowerOfTwo = (n) =>{
+
+    if(n===1) return true
+    console.log("Hello")
+
+    if(n<1 || n%2 !==0) return false  // Checking interger is odd or even is one of the best condition to check the power of two. This saved calling function again if the integer is less than 1 or an odd number
+
+    return isPowerOfTwo(n/2)
+    
+}
+
+console.log(isPowerOfTwo(17))
