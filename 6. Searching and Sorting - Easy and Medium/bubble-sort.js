f9bbaf101eec43778783627df825402c450c38d6
Suraj Mourya <mouryasuraj6266444@gmail.com>
2026-08-22 01:22:33 +0530

    tracked swapping is happened or not

--- 6. Searching and Sorting - Easy and Medium/bubble-sort.js
+++ 6. Searching and Sorting - Easy and Medium/bubble-sort.js
@@ -2,7 +2,7 @@
 
 // -----------Tried------------
 
-const arr  = [5,3,7,1,9,0]
+const arr  = [5,2,4,1,0,3,6,9,8]
 
 const bubbleSort = (arr) =>{
 
@@ -24,9 +24,62 @@ const bubbleSort = (arr) =>{
     // Space Complexity = O(1)  
 }
 
-console.log(bubbleSort(arr))
+// console.log(bubbleSort(arr))
 
 
 
-// -----------Tried------------
+//------------Tried Optimised way-----------
 
+const bubbleSort2 = (arr) =>{
+
+    let p = arr.length
+
+    for (let i = 0; i < arr.length; i++) {
+
+        console.log("Hello")
+        
+        for(let j=0; j<p; j++){
+            console.log("Hello")
+            if(arr[j]>arr[j+1]){
+                let temp = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = temp
+            }
+        }
+        
+        p = p - 1
+    }
+
+    return arr
+
+}
+
+// console.log(bubbleSort2(arr))
+
+
+// Little optimized way by adding is Swap 
+
+
+const bubbleSort3 = (arr) =>{
+
+    let n = arr.length
+
+    for (let i = 0; i < n-1; i++) {
+        let isSwapped = false
+        for(let j=0; j<n-1-i; j++){
+            if(arr[j]>arr[j+1]){
+                let temp = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = temp
+                isSwapped = true
+            }
+        }
+        if(!isSwapped) break;
+    }
+
+    return arr
+
+
+}
+
+console.log(bubbleSort3(arr))
