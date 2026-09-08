545a2f51a9ad319625d7c30392f06c7dac6234a3
Suraj Mourya <mouryasuraj6266444@gmail.com>
2026-09-09 00:03:49 +0530

    implement linked list cycle in a better way using floyd's algorithm

--- 7. Linked List - Easy and Medium/linked-list-cycle.js
+++ 7. Linked List - Easy and Medium/linked-list-cycle.js
@@ -21,5 +21,27 @@ const linkedListCycle = (head) =>{
         seenNode.add(curr)
         curr = curr.next
     }
+
+    // Time Complexity: O(n)
+    // Space Complexity: O(n)
     return false; // if code react at this line then its not a linked list cycle
+}
+
+
+// Better way using Floyd's Algorithm which is also known as Tortois and Hare algorithm
+// using two pointer, slow and fast
+
+const linkedListCycleBetter = (head) =>{
+    let slow = head;
+    let fast = head;
+
+    while(fast && fast.next){
+        slow = slow.next
+        fast = fast.next.next
+
+        if(slow === fast){
+            return true
+        }
+    }
+    return false
 }
