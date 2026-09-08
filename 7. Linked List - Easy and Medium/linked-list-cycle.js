f90f55e593ef99153d0fe40b8241e8fd936de560
Suraj Mourya <mouryasuraj6266444@gmail.com>
2026-09-09 00:20:54 +0530

    implement detact cycle - a follow up question after linked list cycle

--- 7. Linked List - Easy and Medium/linked-list-cycle.js
+++ 7. Linked List - Easy and Medium/linked-list-cycle.js
@@ -45,3 +45,26 @@ const linkedListCycleBetter = (head) =>{
     }
     return false
 }
+
+
+// A follow question to detect when cycle starts
+const DetectCycle = (head) =>{
+    let slow = head;
+    let fast = head;
+
+    while(fast && fast.next){
+        slow = slow.next
+        fast = fast.next.next
+
+        if(slow === fast){
+            break;
+        }
+    }
+    if(!fast || !fast.next) return null
+    slow = head
+    while(slow!==fast){
+        slow = slow.next
+        fast = fast.next
+    }
+    return slow
+}
