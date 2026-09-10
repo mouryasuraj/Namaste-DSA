cdceb1647100bfa12edb5a74d4523a74cc1c3729
Suraj Mourya <mouryasuraj6266444@gmail.com>
2026-09-10 22:59:38 +0530

    handle edge cased in linkedlist cycle using floyd's algorithm

--- 7. Linked List - Easy and Medium/linked-list-cycle.js
+++ 7. Linked List - Easy and Medium/linked-list-cycle.js
@@ -32,18 +32,18 @@ const linkedListCycle = (head) =>{
 // using two pointer, slow and fast
 
 const linkedListCycleBetter = (head) =>{
+    if(!head || !head.next) return false
     let slow = head;
-    let fast = head;
+    let fast = head.next;
 
-    while(fast && fast.next){
+    while(slow !== fast){
+        if(!fast || !fast.next) return false
         slow = slow.next
         fast = fast.next.next
-
-        if(slow === fast){
-            return true
-        }
     }
-    return false
+    return true
+    // Time Complexity: O(n)
+    // Space Complexity: O(n)
 }
 
 
