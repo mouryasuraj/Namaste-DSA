5f2ccff3a84ae2de3b2252c576ce5800390664fa
Suraj Mourya <mouryasuraj6266444@gmail.com>
2026-09-15 23:28:01 +0530

    implement palindrome linked list - aproach 2 without array

--- 7. Linked List - Easy and Medium/palindrome-linked-list.js
+++ 7. Linked List - Easy and Medium/palindrome-linked-list.js
@@ -34,3 +34,49 @@ const palindromeLinkedList = (head) =>{
     // Time Complexity = O(n)+O(n/2) ==> O(n)
     // Space Complexity = O(n)
 }
+
+// Follow up question - Can you solve this question with time = O(n) and space = O(1)
+// Approach 2 - 
+
+const palindromeLinkedListOptm = (head) =>{
+    if(head && !head.next) return true;
+
+    // Find Mid linked list
+    let slow = fast = head
+
+    while(fast && fast.next){
+        slow = slow.next;
+        fast = fast.next.next
+    }
+    // after the above loops runs slow is the middle linked list
+    // now we have to revered the linked list from middle
+    // We will consider slow as current element
+
+    let prev = null
+
+    while(slow){
+        let temp = slow.next
+        slow.next = prev
+        prev = slow
+        slow = temp
+    }
+    // after the above loop runs prev val indicate the last element and linked list is reversed 
+
+    // Now we have to compare the first half with second half
+    // prev = lastElement
+    // head = firstElement
+
+    let firstElement = head
+
+    while(prev){ // we are running this loops till prev is not null because reversed linked list is pointing to null at the end
+        if(head.val !== prev.val) return false
+
+        prev = prev.next
+        firstElement = firstElement.next
+
+    }
+
+    return true
+    // Time Complexity = O(n)+O(n)+O(n) ==> O(n)
+    // Space Complexity = O(1)
+}
