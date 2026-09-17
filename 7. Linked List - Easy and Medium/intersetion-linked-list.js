8bfcfad985d9552751318899e54c64b4c31a4a31
Suraj Mourya <mouryasuraj6266444@gmail.com>
2026-09-17 22:14:54 +0530

    implement intersection linked list using hash

--- 7. Linked List - Easy and Medium/intersetion-linked-list.js
+++ 7. Linked List - Easy and Medium/intersetion-linked-list.js
@@ -24,3 +24,25 @@ const intersectionLinkedList = (headA, headB) => {
 
 
 // Approach 2 using Hash map
+const intersectionLinkedListUsingSet = (headA, headB) =>{
+    if(!headA || !headB) return null
+
+    // Store headA nodes
+    const store = new Set()
+
+    // Iterate through headB and store all the nodes in Set
+    while(headB){
+        store.add(headB)
+        headB = headB.next
+    }
+
+    // Now, Iterate through headA and check headA node is present or not in store
+    while(headA){
+        if(store.has(headA)) return headA
+        headA = headA.next
+    }
+
+    // Time Complexity --> O(m + n)
+    // Space complexity --> O(n)
+    return null // if no intersection then return null
+}
